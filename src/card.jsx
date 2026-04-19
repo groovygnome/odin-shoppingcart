c4106123f4b02fb319c3fb254375b0098aba5b5c
groovygnome <jrp810@proton.me>
2026-04-18 21:17:57 -0400

    add styling to cart buttons, implement changing specific quantity of item

--- src/card.jsx
+++ src/card.jsx
@@ -2,12 +2,13 @@ import add from './assets/add.svg'
 import addToCart from './assets/addtocart.svg'
 import remove from './assets/remove.svg'
 import trash from './assets/delete.svg'
+import { useState } from 'react'
 
 function Card({ title, description, img = '', isProduct = false, updateCart = null, getProductAmt = null, price = 0, id = 0 }) {
 
   if (isProduct) {
     return (
-      <div className='shoppingCard'>`
+      <div className='shoppingCard'>
         <img src={img} />
         <h3>{title}</h3>
         <p>{description}</p>
@@ -26,22 +27,24 @@ function Card({ title, description, img = '', isProduct = false, updateCart = nu
 
 function CartButton({ id, updateCart, getProductAmt }) {
   let productAmt = getProductAmt(id);
+  let [newAmt, setNewAmt] = useState();
   if (productAmt === 0 || !productAmt) {
-    return (<button onClick={() => updateCart(id, { increment: true })}><img src={addToCart} alt='Add to Cart' /></button>)
-  } else if (productAmt > 1) {
     return (
       <div className='cartButton'>
-        <button onClick={() => updateCart(id, { decrement: true })}><img src={remove} alt='Remove from Cart' /></button>
-        <p>{productAmt}</p>
-        <button onClick={() => updateCart(id, { increment: true })}><img src={add} alt='One More' /></button>
+        <button className='addtocartButton' onClick={() => updateCart(id, { increment: true })}><img src={addToCart} alt='Add to Cart' /></button>
       </div>
     )
   } else {
     return (
       <div className='cartButton'>
-        <button onClick={() => updateCart(id, { decrement: true })}><img src={trash} alt='One Less' /></button>
-        <p>{productAmt}</p>
-        <button onClick={() => updateCart(id, { increment: true })}><img src={add} alt='One More' /></button>
+        <div className='cbUpper'>
+          <button onClick={() => updateCart(id, { decrement: true })}><img src={productAmt === 1 ? trash : remove} alt='Remove from Cart' /></button>
+          <input type='number' min='0' onChange={(e) => setNewAmt(Number(e.target.value))} placeholder={productAmt} />
+          <button onClick={() => updateCart(id, { increment: true })}><img src={add} alt='One More' /></button>
+        </div>
+        <div className='cbLower'>
+          <button onClick={() => updateCart(id, { quantity: newAmt })}>UPDATE CART</button>
+        </div>
       </div>
     )
   }
